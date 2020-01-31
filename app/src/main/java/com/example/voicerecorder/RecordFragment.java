17963ddad4c98c08e3cc974771348165a3578dc5
JeZ <JeZ@DESKTOP-MEITL6E>
2020-01-31 19:02:51 +0300

    play_list

--- app/src/main/java/com/example/voicerecorder/RecordFragment.java
+++ app/src/main/java/com/example/voicerecorder/RecordFragment.java
@@ -3,11 +3,16 @@ package com.example.voicerecorder;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
@@ -15,6 +20,8 @@ import android.view.ViewGroup;
  */
 public class RecordFragment extends Fragment {
 
+    private NavController navController;
+    private ImageView listBtn;
 
     public RecordFragment() {
         // Required empty public constructor
@@ -28,4 +35,17 @@ public class RecordFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_record, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+        listBtn = view.findViewById(R.id.record_list_btn);
+        listBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_recordFragment_to_audioListFragment);
+            }
+        });
+    }
 }

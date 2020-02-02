573ddd586fbff72f27035abce94b1627d303e3d5
JeZ <JeZ@DESKTOP-MEITL6E>
2020-02-02 13:53:20 +0300

    add audio list

--- app/src/main/java/com/example/voicerecorder/AudioListFragment.java
+++ app/src/main/java/com/example/voicerecorder/AudioListFragment.java
@@ -3,11 +3,20 @@ package com.example.voicerecorder;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+import java.io.File;
 
 
 /**
@@ -15,6 +24,11 @@ import android.view.ViewGroup;
  */
 public class AudioListFragment extends Fragment {
 
+    private ConstraintLayout playerSheet;
+    private BottomSheetBehavior bottomSheetBehavior;
+    private RecyclerView audioList;
+    private File[] allFiles;
+    private AudioListAdapter audioListAdapter;
 
     public AudioListFragment() {
         // Required empty public constructor
@@ -28,4 +42,36 @@ public class AudioListFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_audio_list, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        playerSheet = view.findViewById(R.id.player_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(playerSheet);
+        audioList = view.findViewById(R.id.audio_list_view);
+
+
+
+        String path = getActivity().getExternalFilesDir("/").getAbsolutePath();
+        File directory = new File(path);
+        allFiles = directory.listFiles();
+
+        audioListAdapter = new AudioListAdapter(allFiles);
+        audioList.setHasFixedSize(true);
+        audioList.setLayoutManager(new LinearLayoutManager(getContext()));
+        audioList.setAdapter(audioListAdapter);
+        bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_COLLAPSED){
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+    }
 }

d137dc418865ecb9f82badb14b62c22b01cc4722
JeZ <JeZ@DESKTOP-MEITL6E>
2020-02-03 21:11:02 +0300

    add audioplayer

--- app/src/main/java/com/example/voicerecorder/AudioListFragment.java
+++ app/src/main/java/com/example/voicerecorder/AudioListFragment.java
@@ -1,10 +1,13 @@
 package com.example.voicerecorder;
 
 
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -13,22 +16,32 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import java.io.File;
+import java.io.IOException;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AudioListFragment extends Fragment {
+public class AudioListFragment extends Fragment implements AudioListAdapter.onItemListClick {
 
     private ConstraintLayout playerSheet;
     private BottomSheetBehavior bottomSheetBehavior;
     private RecyclerView audioList;
     private File[] allFiles;
     private AudioListAdapter audioListAdapter;
+    private MediaPlayer mediaPlayer =null;
+    private boolean isPlaying = false;
+    private File fileToPlay;
+
+    private ImageButton playBtn;
+    private TextView playerHeader;
+    private TextView playerFilename;
 
     public AudioListFragment() {
         // Required empty public constructor
@@ -49,17 +62,28 @@ public class AudioListFragment extends Fragment {
         playerSheet = view.findViewById(R.id.player_sheet);
         bottomSheetBehavior = BottomSheetBehavior.from(playerSheet);
         audioList = view.findViewById(R.id.audio_list_view);
-
+        playBtn = view.findViewById(R.id.player_btn_play);
+        playerHeader = view.findViewById(R.id.player_header_title);
+        playerFilename = view.findViewById(R.id.player_file_name);
 
 
         String path = getActivity().getExternalFilesDir("/").getAbsolutePath();
         File directory = new File(path);
         allFiles = directory.listFiles();
 
-        audioListAdapter = new AudioListAdapter(allFiles);
+        audioListAdapter = new AudioListAdapter(allFiles,this);
         audioList.setHasFixedSize(true);
         audioList.setLayoutManager(new LinearLayoutManager(getContext()));
         audioList.setAdapter(audioListAdapter);
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stopAUDIO();
+                playerHeader.setText(R.string.finished);
+            }
+        });
+
         bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
             public void onStateChanged(@NonNull View bottomSheet, int newState) {
@@ -71,6 +95,56 @@ public class AudioListFragment extends Fragment {
             @Override
             public void onSlide(@NonNull View bottomSheet, float slideOffset) {
 
+            }
+        });
+    }
+
+    @Override
+    public void onClickListener(File file, int position) {
+        if (isPlaying){
+            stopAUDIO();
+
+        }else {
+            fileToPlay = file;
+            playAudio(fileToPlay);
+
+        }
+    }
+
+
+    private void stopAUDIO() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            playBtn.setImageDrawable(getActivity().getResources().getDrawable(R.drawable.player_play_btn,null));
+            playerHeader.setText(R.string.stoped);
+        }
+        isPlaying = false;
+    }
+
+
+    private void playAudio(File fileToPlay) {
+        mediaPlayer = new MediaPlayer();
+
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        try {
+            mediaPlayer.setDataSource(fileToPlay.getAbsolutePath());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            playBtn.setImageDrawable(getActivity().getResources().getDrawable(R.drawable.player_pause_btn,null));
+            playerFilename.setText(fileToPlay.getName());
+            playerHeader.setText(R.string.playing);
+        }
+
+        isPlaying = true;
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stopAUDIO();
+                playerHeader.setText(R.string.finished);
             }
         });
     }
